6e0b23c50902220a441595ef0f8176029fb582ad
junwen tang <tangjunwena1mu@icloud.com>
2023-12-08 16:39:07 +0800

    init

---
+++ SerializationDemo/JsonHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateRouteEngine.Core.Utilities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class JsonHelper
+    {
+        /// <summary>
+        /// 从一个对象信息生成Json串
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ObjectToJson(object obj)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            MemoryStream stream = new MemoryStream();
+            serializer.WriteObject(stream, obj);
+            byte[] dataBytes = new byte[stream.Length];
+            stream.Position = 0;
+            stream.Read(dataBytes, 0, (int)stream.Length);
+            return Encoding.UTF8.GetString(dataBytes);
+        }
+    }
+}
---
+++ SerializationDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using RateRouteEngine.Core.Utilities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is a result of serialization performance test:");
+
+        // 创建测试数据
+        var person = new Person
+        {
+            Name = "John Doe",
+            Age = 30,
+            Email = "john@example.com"
+        };
+
+        // 预热（JIT编译优化）
+        var dataContractJsonSerializer = JsonHelper.ObjectToJson(person);
+        var systemTextJson = System.Text.Json.JsonSerializer.Serialize(person);
+
+        // 测试 DataContractJsonSerializer
+        var sw = Stopwatch.StartNew();
+        for (int i = 0; i < 10000; i++)
+        {
+            JsonHelper.ObjectToJson(person);
+        }
+        sw.Stop();
+        Console.WriteLine($"DataContractJsonSerializer: {sw.ElapsedMilliseconds} ms");
+
+        // 测试 System.Text.Json
+        sw.Restart();
+        for (int i = 0; i < 10000; i++)
+        {
+            System.Text.Json.JsonSerializer.Serialize(person);
+        }
+        sw.Stop();
+        Console.WriteLine($"System.Text.Json: {sw.ElapsedMilliseconds} ms");
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+}
